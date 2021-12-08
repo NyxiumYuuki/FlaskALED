55ca9907a9710af133d7f6f928774e3d1ed5acb1
NyxiumYuuki <yuki@vachot.fr>
2021-12-08 10:01:08 +0100

    Update: isAdmin field + nullable=False

--- backend/application/logs_model.py
+++ backend/application/logs_model.py
@@ -5,12 +5,12 @@ class Logs(db.Model):
     __bind_key__ = 'logs'
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date())
-    user = db.Column(db.String())
-    ip = db.Column(db.String())
-    table = db.Column(db.String())
-    action = db.Column(db.String())
-    status = db.Column(db.String())
+    date = db.Column(db.Date(), nullable=False)
+    user = db.Column(db.String(), nullable=False)
+    ip = db.Column(db.String(), nullable=False)
+    table = db.Column(db.String(), nullable=False)
+    action = db.Column(db.String(), nullable=False)
+    status = db.Column(db.String(), nullable=False)
 
     def __init__(self, date, user, ip, table, action, status):
         self.date = date
