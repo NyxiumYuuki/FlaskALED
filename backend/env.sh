22664e3f468ac5833d188c3cd7f9b8fb20faf9c1
NyxiumYuuki <yuki@vachot.fr>
2021-12-08 13:07:49 +0100

    Update: Env SH Example

--- backend/env.sh
+++ backend/env.sh
@@ -1,4 +1,6 @@
 export FLASK_APP=run.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1
 export PYTHONUNBUFFERED=1
 export DATABASE_URL_1=postgresql://localhost:5433/users
 export DATABASE_URL_2=postgresql://localhost:5433/logs
