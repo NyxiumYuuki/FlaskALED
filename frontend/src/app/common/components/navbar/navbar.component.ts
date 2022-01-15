0d6fde6aba50bc6521c5edc72945930eca2046c7
Yûki Vachot <yuki@vachot.fr>
2022-01-15 18:59:28 +0100

    Update: Docker dev + logout on frontend

--- frontend/src/app/common/components/navbar/navbar.component.ts
+++ frontend/src/app/common/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ProfilService} from "../../services/profil/profil.service";
+import {MessageService} from "../../services/message/message.service";
 
 @Component({
   selector: 'app-navbar',
@@ -10,14 +11,22 @@ export class NavbarComponent implements OnInit
 {
     @Input() pour = "login";
 
-    constructor(private profilService: ProfilService) { }
+    constructor(private profilService: ProfilService, private messageService: MessageService) { }
 
     ngOnInit(): void {}
 
     onDeconnexion(): void
     {
+        this.messageService
+            .delete('logout')
+            .subscribe(retour => this.onDeconnexionCallback(retour), err => this.onDeconnexionCallback(err));
         this.profilService.setId(-1);
         this.profilService.setIsAdmin(false);
+    }
+
+    onDeconnexionCallback(retour: any): void
+    {
+        if(retour.status !== "success") console.log(retour);
     }
 
 }
