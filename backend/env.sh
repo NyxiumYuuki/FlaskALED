d6d0d4316714bd49ac6bc47986ea2e1f534894bc
Yûki Vachot <yuki@vachot.fr>
2022-01-24 19:59:07 +0100

    Create: db2

--- backend/env.sh
+++
@@ -1,6 +0,0 @@
-export FLASK_APP=app.py
-export FLASK_ENV=development
-export FLASK_DEBUG=1
-export PYTHONUNBUFFERED=1
-export DATABASE_URL_USERS=postgresql://flaskaled1:aled1@localhost:5433/flaskaledDb1
-export DATABASE_URL_LOGS=postgresql://flaskaled2:aled2@localhost:5434/flaskaledDb2
