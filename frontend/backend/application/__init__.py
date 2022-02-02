4dc87f579ccfefa2d7170f9b9902c4ba6eb9556f
Baptiste Peupier <60870677+BaptistePeupier@users.noreply.github.com>
2022-02-02 14:00:50 +0100

    voilàvoilà

--- frontend/backend/application/__init__.py
+++ frontend/backend/application/__init__.py
@@ -7,7 +7,7 @@ def create_app(flask_env='development'):
     app = Flask(__name__, instance_relative_config=False, static_url_path='')
     origin = app.config.get('ALLOW_ORIGIN')
     if origin is None:
-        origin = ['http://10.1.1.10', 'http://10.1.5.10', 'http://10.1.1.10:4200', 'http://10.1.5.10:4200']
+        origin = ['http://10.1.1.10']
     CORS(app, supports_credentials=True, origins=origin)
     if flask_env == 'production':
         app.config.from_object("config.ProductionConfig")
